e1e6ce171306e3674e25c8b1493e5da0e0328882
sn0wden <92636551+Defen21@users.noreply.github.com>
2025-12-13 23:36:52 +0700

    update progress

--- watchlist.h
+++ watchlist.h
@@ -1,5 +1,6 @@
 #ifndef HEADER_H_INCLUDED
 #define HEADER_H_INCLUDED
+
 #include <iostream>
 #include <string>
 using namespace std;
@@ -68,25 +69,11 @@ adrRelation allocateRelation(adrMovie M);
 
 void insertFirstGenre(ListGenre &LG, adrGenre P);
 void insertLastGenre(ListGenre &LG, adrGenre P);
-void insertAfterGenre(adrGenre Prec, adrGenre P);
-
-/* ================= DELETE GENRE ================= */
-
-void deleteFirstGenre(ListGenre &LG, adrGenre &P);
-void deleteLastGenre(ListGenre &LG, adrGenre &P);
-void deleteAfterGenre(adrGenre Prec, adrGenre &P);
 
 /* ================= INSERT MOVIE ================= */
 
 void insertFirstMovie(ListMovie &LM, adrMovie P);
 void insertLastMovie(ListMovie &LM, adrMovie P);
-void insertAfterMovie(adrMovie Prec, adrMovie P);
-
-/* ================= DELETE MOVIE ================= */
-
-void deleteFirstMovie(ListMovie &LM, adrMovie &P);
-void deleteLastMovie(ListMovie &LM, adrMovie &P);
-void deleteAfterMovie(adrMovie Prec, adrMovie &P);
 
 /* ================= RELATION ================= */
 
@@ -101,13 +88,11 @@ adrMovie findMovie(ListMovie LM, string title);
 
 void showAllGenres(ListGenre LG);
 void showAllMovies(ListMovie LM);
-void showMoviesOfGenre(adrGenre G);
 void showAllGenreWithMovies(ListGenre LG);
 void showMovieWithGenres(ListGenre LG, ListMovie LM);
 
 /* ================= COUNT ================= */
 
-int countRelationGenre(adrGenre G);
 int countRelationMovie(ListGenre LG, adrMovie M);
 int countUnrelatedMovies(ListGenre LG, ListMovie LM);
 

b9ce8d14b88d07944f842a9da5957aa9d55ed554
supercupapi <zaidansr1919@gmail.com>
2025-12-20 17:42:48 +0700

    Update watchlist.cpp

--- watchlist.cpp
+++ watchlist.cpp
@@ -1,144 +1,225 @@
 #include "watchlist.h"
+#include <iostream>
+using namespace std;
 
-/* ================= CREATE ================= */
+/* ========= CREATE ========= */
+void createMovieList(ListMovie &LM) {
+    LM.first = nullptr;
+}
 
 void createGenreList(ListGenre &LG) {
     LG.first = nullptr;
 }
 
-void createMovieList(ListMovie &LM) {
-    LM.first = nullptr;
+/* ========= ALLOCATE ========= */
+Movie* allocateMovie(string title, int year, float rating) {
+    Movie* P = new Movie;
+    P->title = title;
+    P->year = year;
+    P->rating = rating;
+    P->next = nullptr;
+    return P;
 }
 
-/* ================= ALLOCATE ================= */
-
-adrGenre allocateGenre(string name) {
-    adrGenre P = new elmGenre;
-    P->info.name = name;
+Genre* allocateGenre(string name) {
+    Genre* P = new Genre;
+    P->name = name;
     P->next = nullptr;
     P->relFirst = nullptr;
     return P;
 }
 
-adrMovie allocateMovie(string title, int year, float rating) {
-    adrMovie P = new elmMovie;
-    P->info.title = title;
-    P->info.year = year;
-    P->info.rating = rating;
-    P->next = nullptr;
-    return P;
-}
-
-adrRelation allocateRelation(adrMovie M) {
-    adrRelation R = new elmRelation;
-    R->child = M;
+Relation* allocateRelation(Movie* M) {
+    Relation* R = new Relation;
+    R->movie = M;
     R->next = nullptr;
     return R;
 }
 
-/* ================= INSERT ================= */
-
-void insertFirstGenre(ListGenre &LG, adrGenre P) {
+/* ========= INSERT GENRE ========= */
+void insertFirstGenre(ListGenre &LG, Genre* P) {
     P->next = LG.first;
     LG.first = P;
 }
 
-void insertLastGenre(ListGenre &LG, adrGenre P) {
+void insertLastGenre(ListGenre &LG, Genre* P) {
     if (LG.first == nullptr)
         LG.first = P;
     else {
-        adrGenre Q = LG.first;
+        Genre* Q = LG.first;
         while (Q->next != nullptr)
             Q = Q->next;
         Q->next = P;
     }
 }
 
-void insertFirstMovie(ListMovie &LM, adrMovie P) {
+void insertAfterGenre(Genre* Prec, Genre* P) {
+    if (Prec != nullptr) {
+        P->next = Prec->next;
+        Prec->next = P;
+    }
+}
+
+/* ========= INSERT MOVIE ========= */
+void insertFirstMovie(ListMovie &LM, Movie* P) {
     P->next = LM.first;
     LM.first = P;
 }
 
-void insertLastMovie(ListMovie &LM, adrMovie P) {
+void insertLastMovie(ListMovie &LM, Movie* P) {
     if (LM.first == nullptr)
         LM.first = P;
     else {
-        adrMovie Q = LM.first;
+        Movie* Q = LM.first;
         while (Q->next != nullptr)
             Q = Q->next;
         Q->next = P;
     }
 }
 
-/* ================= RELATION ================= */
-
-void connectGenreMovie(adrGenre G, adrMovie M) {
-    adrRelation R = allocateRelation(M);
-    if (G->relFirst == nullptr)
-        G->relFirst = R;
-    else {
-        adrRelation Q = G->relFirst;
-        while (Q->next != nullptr)
-            Q = Q->next;
-        Q->next = R;
+void insertAfterMovie(Movie* Prec, Movie* P) {
+    if (Prec != nullptr) {
+        P->next = Prec->next;
+        Prec->next = P;
     }
 }
 
-/* ================= FIND ================= */
-
-adrGenre findGenre(ListGenre LG, string name) {
-    adrGenre P = LG.first;
-    while (P != nullptr) {
-        if (P->info.name == name)
-            return P;
-        P = P->next;
-    }
-    return nullptr;
+/* ========= RELATION ========= */
+void connectGenreMovie(Genre* G, Movie* M) {
+    Relation* R = allocateRelation(M);
+    R->next = G->relFirst;
+    G->relFirst = R;
 }
 
-adrMovie findMovie(ListMovie LM, string title) {
-    adrMovie P = LM.first;
-    while (P != nullptr) {
-        if (P->info.title == title)
-            return P;
-        P = P->next;
+/* ========= DELETE GENRE ========= */
+void deleteFirstGenre(ListGenre &LG, Genre* &P) {
+    if (LG.first != nullptr) {
+        P = LG.first;
+        LG.first = P->next;
+        P->next = nullptr;
     }
-    return nullptr;
 }
 
-/* ================= SHOW ================= */
+void deleteLastGenre(ListGenre &LG, Genre* &P) {
+    if (LG.first != nullptr) {
+        Genre* Q = LG.first;
+        if (Q->next == nullptr) {
+            P = Q;
+            LG.first = nullptr;
+        } else {
+            while (Q->next->next != nullptr)
+                Q = Q->next;
+            P = Q->next;
+            Q->next = nullptr;
+        }
+    }
+}
 
-void showAllGenres(ListGenre LG) {
-    adrGenre G = LG.first;
+void deleteAfterGenre(Genre* Prec, Genre* &P) {
+    if (Prec != nullptr && Prec->next != nullptr) {
+        P = Prec->next;
+        Prec->next = P->next;
+        P->next = nullptr;
+    }
+}
+
+/* ========= DELETE MOVIE ========= */
+void deleteFirstMovie(ListMovie &LM, Movie* &P) {
+    if (LM.first != nullptr) {
+        P = LM.first;
+        LM.first = P->next;
+        P->next = nullptr;
+    }
+}
+
+void deleteLastMovie(ListMovie &LM, Movie* &P) {
+    if (LM.first != nullptr) {
+        Movie* Q = LM.first;
+        if (Q->next == nullptr) {
+            P = Q;
+            LM.first = nullptr;
+        } else {
+            while (Q->next->next != nullptr)
+                Q = Q->next;
+            P = Q->next;
+            Q->next = nullptr;
+        }
+    }
+}
+
+void deleteAfterMovie(Movie* Prec, Movie* &P) {
+    if (Prec != nullptr && Prec->next != nullptr) {
+        P = Prec->next;
+        Prec->next = P->next;
+        P->next = nullptr;
+    }
+}
+
+/* ========= DELETE RELATION ========= */
+void deleteRelation(Genre* G, Movie* M) {
+    Relation* R = G->relFirst;
+    Relation* prev = nullptr;
+
+    while (R != nullptr && R->movie != M) {
+        prev = R;
+        R = R->next;
+    }
+
+    if (R != nullptr) {
+        if (prev == nullptr)
+            G->relFirst = R->next;
+        else
+            prev->next = R->next;
+        delete R;
+    }
+}
+
+/* ========= FIND ========= */
+Genre* findGenre(ListGenre LG, string name) {
+    Genre* G = LG.first;
     while (G != nullptr) {
-        cout << "- " << G->info.name << endl;
+        if (G->name == name)
+            return G;
+        G = G->next;
+    }
+    return nullptr;
+}
+
+Movie* findMovie(ListMovie LM, string title) {
+    Movie* M = LM.first;
+    while (M != nullptr) {
+        if (M->title == title)
+            return M;
+        M = M->next;
+    }
+    return nullptr;
+}
+
+/* ========= SHOW ========= */
+void showAllGenres(ListGenre LG) {
+    Genre* G = LG.first;
+    while (G != nullptr) {
+        cout << G->name << endl;
         G = G->next;
     }
 }
 
 void showAllMovies(ListMovie LM) {
-    adrMovie M = LM.first;
+    Movie* M = LM.first;
     while (M != nullptr) {
-        cout << "- " << M->info.title
-             << " (" << M->info.year << ")"
-             << " | Rating: " << M->info.rating << endl;
+        cout << M->title << " (" << M->year
+             << ") rating " << M->rating << endl;
         M = M->next;
     }
 }
 
 void showAllGenreWithMovies(ListGenre LG) {
-    adrGenre G = LG.first;
+    Genre* G = LG.first;
     while (G != nullptr) {
-        cout << "Genre: " << G->info.name << endl;
-        adrRelation R = G->relFirst;
-
-        if (R == nullptr)
-            cout << "  (Tidak ada movie)" << endl;
-
+        cout << G->name << ":\n";
+        Relation* R = G->relFirst;
         while (R != nullptr) {
-            cout << "  - " << R->child->info.title
-                 << " (" << R->child->info.year << ")"
-                 << " | Rating: " << R->child->info.rating << endl;
+            cout << "  - " << R->movie->title << endl;
             R = R->next;
         }
         G = G->next;
@@ -146,44 +227,53 @@ void showAllGenreWithMovies(ListGenre LG) {
 }
 
 void showMovieWithGenres(ListGenre LG, ListMovie LM) {
-    adrMovie M = LM.first;
+    Movie* M = LM.first;
     while (M != nullptr) {
-        cout << "Movie: " << M->info.title
-             << " (" << M->info.year << ")"
-             << " | Rating: " << M->info.rating
-             << " | Genre: ";
-
-        bool found = false;
-        adrGenre G = LG.first;
-
-        while (G != nullptr) {
-            adrRelation R = G->relFirst;
-            while (R != nullptr) {
-                if (R->child == M) {
-                    cout << G->info.name << " ";
-                    found = true;
-                }
-                R = R->next;
-            }
-            G = G->next;
-        }
-
-        if (!found)
-            cout << "Tidak ada";
-        cout << endl;
+        cout << M->title << ":\n";
+        showGenresOfMovie(LG, M);
         M = M->next;
     }
 }
 
-/* ================= COUNT ================= */
+void showGenresOfMovie(ListGenre LG, Movie* M) {
+    Genre* G = LG.first;
+    bool found = false;
 
-int countRelationMovie(ListGenre LG, adrMovie M) {
-    int count = 0;
-    adrGenre G = LG.first;
     while (G != nullptr) {
-        adrRelation R = G->relFirst;
+        Relation* R = G->relFirst;
         while (R != nullptr) {
-            if (R->child == M)
+            if (R->movie == M) {
+                cout << "  - " << G->name << endl;
+                found = true;
+            }
+            R = R->next;
+        }
+        G = G->next;
+    }
+
+    if (!found)
+        cout << "  (tidak ada genre)\n";
+}
+
+/* ========= COUNT ========= */
+int countRelationGenre(Genre* G) {
+    int count = 0;
+    Relation* R = G->relFirst;
+    while (R != nullptr) {
+        count++;
+        R = R->next;
+    }
+    return count;
+}
+
+int countRelationMovie(ListGenre LG, Movie* M) {
+    int count = 0;
+    Genre* G = LG.first;
+
+    while (G != nullptr) {
+        Relation* R = G->relFirst;
+        while (R != nullptr) {
+            if (R->movie == M)
                 count++;
             R = R->next;
         }
@@ -194,11 +284,24 @@ int countRelationMovie(ListGenre LG, adrMovie M) {
 
 int countUnrelatedMovies(ListGenre LG, ListMovie LM) {
     int count = 0;
-    adrMovie M = LM.first;
+    Movie* M = LM.first;
+
     while (M != nullptr) {
         if (countRelationMovie(LG, M) == 0)
             count++;
         M = M->next;
     }
     return count;
+}
+
+/* ========= EDIT ========= */
+void editRelation(Genre* G, Movie* oldM, Movie* newM) {
+    Relation* R = G->relFirst;
+    while (R != nullptr) {
+        if (R->movie == oldM) {
+            R->movie = newM;
+            return;
+        }
+        R = R->next;
+    }
 }
